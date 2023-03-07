2f2a25d32fbd2cfa8ef5e925172fed8c47d6ed16
JordiViCa <jvila@3ktec.com>
2023-03-07 20:29:24 +0100

    [Feat] Add routes to publicArea

--- src/app/public-area/public-area-routing.module.ts
+++ src/app/public-area/public-area-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AffiliatesComponent } from './affiliates/affiliates.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LandingComponent
+  },
+  {
+    path: "about",
+    component: AboutUsComponent
+  },
+  {
+    path: "affiliates",
+    component: AffiliatesComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
