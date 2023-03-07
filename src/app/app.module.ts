799d400962d383345444e81b52ca45bd10a27158
JordiViCa <jvila@3ktec.com>
2023-03-07 20:28:58 +0100

    [Feat] Add layouts and routes to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ClientAreaComponent } from './layouts/client-area/client-area.component';
+import { PublicAreaComponent } from './layouts/public-area/public-area.component';
+import { WorkAreaComponent } from './layouts/work-area/work-area.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    ClientAreaComponent,
+    PublicAreaComponent,
+    WorkAreaComponent
+  ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
